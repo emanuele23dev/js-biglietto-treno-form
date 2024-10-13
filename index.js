6719b00b30d2ce5b98a30c82783727be763632e9
emanuele23dev <emanuele.meggiarin@libero.it>
2024-10-13 15:54:33 +0200

    final train ticket

--- index.js
+++ index.js
@@ -1,22 +1,129 @@
-const buttonEl = document.getElementById("bottone_prezzo");
+// MILESTONE 1: Iniziamo implementando il programma senza alcuna estetica
+// usando esclusivamente due input e un bottone (non stilizzati),
+// realizziamo le specifiche scritte sopra.
+// La risposta finale (o output) sarà anch’essa da scrivere in console.
 
-buttonEl.addEventListener("click", function calcolaPrezzo() {
-  const user = document.getElementById("user").value;
-  const user_km = parseInt(document.getElementById("user_km").value);
-  const user_age = parseInt(document.getElementById("user_age").value);
+// Scrivere un programma che chieda all’utente:
+// Il numero di chilometri da percorrere
+// Età del passeggero
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del biglietto di viaggio,
+// secondo le seguenti regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
 
-  const ticket = user_km * 0.21;
+const formEl = document.getElementById("ticket-form");
+console.log(formEl);
 
-  if (user_age < 18) {
-    prezzoTicket = ticket - (ticket * 0.2);
-  } else if (user_age > 65) {
-    prezzoTicket = ticket - (ticket * 0.4);
-  } else {
-    prezzoTicket = ticket;
+// const fullName = document.getElementById('Fullname');
+// console.log(fullnameinputEl);
+
+// const km = document.getElementById('km');
+// console.log(kminputEl);
+
+// const ageRange = document.getElementById('ageRange');
+// console.log(ageRange);
+
+const resetBtnEl = document.querySelector(".reset");
+console.log(resetBtnEl);
+
+const ticketSectionEl = document.getElementById("ticket");
+console.log(ticketSectionEl);
+
+const pricePerkm = 0.21;
+console.log(pricePerkm);
+
+formEl.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // console.log(fullnameinputEl.value, Number(kminputEl.value), Number(ageRange.value));
+
+  const fullName = e.target.fullname.value;
+  // - read the value of the input km
+  console.log(fullName);
+
+  const km = Number(e.target.km.value);
+  console.log(km);
+
+  // - read the value of the age range selection
+  const ageRange = e.target.ageRange.value;
+  console.log(ageRange);
+
+  let final_price = pricePerkm * km;
+  console.log(final_price);
+
+  let discount;
+  let offertType = "Biglietto standard";
+
+  if (ageRange === "minor") {
+    discount = 0.2;
+    final_price = final_price - final_price * discount;
+    offertType = "Biglietto Minori";
+  } else if (ageRange === "senior") {
+    discount = 0.4;
+    final_price = final_price - final_price * discount;
+    offertType = "Biglietto Senior";
   }
 
-  const prezzoFinale = prezzoTicket.toFixed(2);
+  console.log(Number(final_price.toFixed(2)));
 
-  const resultEl = document.getElementById("result");
-  resultEl.innerHTML = `Ciao ${user}, percorrendo ${user_km} km, il prezzo del tuo biglietto è di ${prezzoFinale} €`;
+  // update the dom to print the generated ticket
+  // generateTicket()
+
+  // genera numero random per wagon
+  const wagon = generateRandomInteger(1, 10);
+  // genera numero random per cp_code
+  const cp_code = generateRandomInteger(80000, 99000);
+
+  // generate the ticket markup to insert the ticket section
+  const markup = generateTicketMarkup(
+    fullName,
+    offertType,
+    final_price,
+    wagon,
+    cp_code
+  );
+  // update the section innerHTML
+  ticketSectionEl.innerHTML += markup;
 });
+
+function generateTicketMarkup(
+  fullName,
+  offertType,
+  final_price,
+  wagon,
+  cp_code
+) {
+  return `
+ <div class="card mt-5 mb-5">
+          <div class="card-body d-flex gap-5">
+            <div class="passenger bg-dark text-white p-3 rounded">
+              <h2>Nome Passeggero</h2>
+              <h4>${fullName}</h4>
+            </div>
+            <div class="ticket-details d-flex align-items-center">
+              <div>
+                <h6>Offer</h6>
+                <div>${offertType}</div>
+              </div>
+              <div>
+                <h6>Wagon</h6>
+                <div>${wagon}</div>
+              </div>
+              <div>
+                <h6>Cp Code</h6>
+                <div>${cp_code}</div>
+              </div>
+              <div>
+                <h6>Final Price</h6>
+                <div>${final_price}</div>
+              </div>
+            </div>
+          </div>
+        </div>
+`;
+}
+
+function generateRandomInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
